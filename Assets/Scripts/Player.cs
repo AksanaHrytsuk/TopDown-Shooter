e51af0eb3edaecb5a05e515545bbca1fd4a253f0
Oksana <kroshkaksju@gmail.com>
2020-04-17 00:04:23 +0300

    animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    public Transform shootPosition;
+
+    public float fireRate = 1;
+
+    private float nextFire;
+
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && nextFire <= 0)
+        {
+            Instantiate(bulletPrefab, shootPosition.position, transform.rotation);
+            nextFire = fireRate;
+            _animator.SetTrigger("Shoot");
+        }
+
+        if (nextFire > 0)
+        {
+            nextFire -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        // Debug.Log(transform.up);
+       Move();
+       Rotate();
+    }
+
+    void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        
+        _rigidbody2D.velocity = new Vector2(inputX, inputY) * speed;
+        
+        _animator.SetFloat("Speed", _rigidbody2D.velocity.magnitude);
+        
+        // move with upt. transform
+        // Vector3 newPosition = transform.position;
+        // newPosition.x += speed * Time.deltaTime * inputX;
+        // transform.position = newPosition; 
+    }
+
+    void Rotate()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = transform.position - mouseWorldPosition ;
+        transform.up = direction;
+    }
+}
