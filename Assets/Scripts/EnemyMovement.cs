b8733d58e9bfa2be63c0ee700c31227eecebec91
Oksana <kroshkaksju@gmail.com>
2020-04-21 19:56:07 +0300

    new

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Player _player;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+       _player = FindObjectOfType<Player>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+    
+    private void Rotate()
+    {
+        if (_player != null)
+        {
+            Vector2 direction = transform.position - _player.transform.position;
+            transform.up = direction;
+        }
+    }
+}
