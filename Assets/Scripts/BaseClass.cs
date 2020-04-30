799c8d8c08a0dd780aee3514bf144c34d54760f7
Oksana <kroshkaksju@gmail.com>
2020-04-30 18:50:35 +0300

    refact

---
+++ Assets/Scripts/BaseClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public abstract class BaseClass : MonoBehaviour
+{
+    [Header("Config Parameters")] public float health;
+    public float speed;
+    public float damage;
+
+
+    private Player _player;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
+
+    public Player GetPlayer()
+    {
+        return _player;
+    }
+
+    public Rigidbody2D GetRig()
+    {
+        return _rigidbody2D;
+    }
+
+    public Animator GetAnimator()
+    {
+        return _animator;
+    }
+
+    public virtual void GetDamage(float damage)
+    {
+    }
+
+    public virtual void DoDamage(System.Type type)
+    {
+        var obj = GameObject.FindObjectOfType(type);
+        obj.GetDamage(damage);
+        // if (collision != null)
+        // {
+        //     if (collision.CompareTag("Player"))
+        //     {
+        //         FindObjectOfType<Player>().GetDamage(damage);
+        //     }
+        //
+        //     if (collision.CompareTag("Zombie"))
+        //     {
+        //         FindObjectOfType<Zombie>().GetDamage(damage);
+        //     }
+        //     if (collision.CompareTag("Barrel"))
+        //     {
+        //         FindObjectOfType<Barrel>().GetDamage(damage);
+        //     }
+        // }
+    }
+
+    public virtual void Move()
+    {
+    }
+
+    public virtual void Rotate()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : BaseClass
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    public override void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        GetRig().velocity = new Vector2(inputX, inputY) * speed;
+        GetAnimator().SetFloat("Speed", GetRig().velocity.magnitude);
+        
+    }
+    
+    public override void Rotate()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = transform.position - mouseWorldPosition ;
+        transform.up = direction;
+    }
+}
