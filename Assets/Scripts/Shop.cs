a2723cd87606dab0a4886d35542e47a58e10539c
Oksana <kroshkaksju@gmail.com>
2020-05-28 18:48:59 +0300

    Script Shop does not work

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPanel;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        if (collision.CompareTag("Characters"))
+        {
+           shopPanel.SetActive(true);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        shopPanel.SetActive(false);
+        shopPanel = FindObjectOfType<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
